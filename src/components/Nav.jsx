dcd103d09786ddfcc89b659e7c4041a318c12ae0
Rishikesh <rishikeshjagadale19@gmail.com>
2023-08-31 16:23:36 +0530

    NavBar Completed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants/index";
 
 const Nav = () => {
   return (
@@ -14,6 +15,26 @@ const Nav = () => {
             className="m-0 w-[129px] h-[29px]"
           />
         </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray hover:text-black hover:font-medium"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24 hover:text-slate-gray">
+          <a href="/">Sign in</a>
+          <span>/</span>
+          <a href="/">Explore now</a>
+        </div>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="hamburger icon" width={25} height={25} />
+        </div>
       </nav>
     </header>
   );
