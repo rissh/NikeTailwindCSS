48d910071363d2c5d9c4e9d794fa12c3afd9708a
Rishikesh <rishikeshjagadale19@gmail.com>
2023-08-31 15:55:12 +0530

    Rebuilding

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,18 @@
+import { hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
-import { humburger } from "../assets/icons";
 
 const Nav = () => {
   return (
-    <header>
-      <nav>
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
         <a href="/">
-          <img src={headerLogo} alt="Logo" width={130} height={29} />
+          <img
+            src={headerLogo}
+            alt="logo"
+            width={129}
+            height={29}
+            className="m-0 w-[129px] h-[29px]"
+          />
         </a>
       </nav>
     </header>
--- src/sections/index.js
+++ src/sections/index.js
@@ -1,19 +1,19 @@
-import Subscibe from "./Subscribe.jsx";
-import Footer from "./Footer.jsx";
-import CustomerReviews from "./CustomerReviews.jsx";
-import Hero from "./Hero.jsx";
-import PopularProducts from "./PopularProducts.jsx";
-import Services from "./Services.jsx";
-import SpecialOffer from "./SpecialOffer.jsx";
-import SuperQuality from "./SuperQuality.jsx";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
 
 export {
-  Hero,
-  Subscibe,
-  SuperQuality,
-  CustomerReviews,
-  SpecialOffer,
-  Services,
-  PopularProducts,
-  Footer,
-};
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
